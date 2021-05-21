51102ed53afef655748b58ebd0db471c1717defb
Juciélen Lindamaira Crispiniana de Souza <souzajucielen@gmail.com>
2021-05-21 12:23:26 -0300

    Adição de querrys personalizadas no repository

--- src/main/java/br/com/compasso/productMondDB/controller/repository/ProductRepository.java
+++ src/main/java/br/com/compasso/productMondDB/controller/repository/ProductRepository.java
@@ -10,10 +10,12 @@ import br.com.compasso.productMondDB.controller.model.Product;
 
 public interface ProductRepository extends MongoRepository<Product, String>{
 	
-	@Query("select p from Product p where p.price >= :min_price and p.price <= :max_price")
-	List<Product> findByMinMaxPrice(@Param("min_price") Double min_price, @Param("max_price") Double max_price);
+	//@Query("select p from Product p where p.price >= :min_price and p.price <= :max_price")
+	@Query("{ $and : [ { 'price' : { $gte : ?0} } , {'price' : { $lte : ?1 } } ] }")
+	List<Product> findByMinMaxPrice( Double min_price,  Double max_price);
 
-	@Query("select p from Product p where p.price >= :min_price and p.price <= :max_price and p.name = :paramName and p.description = :paramDescription")
+	//@Query("select p from Product p where p.price >= :min_price and p.price <= :max_price and p.name = :paramName and p.description = :paramDescription")
+	@Query("{ $and : [ { 'price' : { $gte : ?0} } , {'price' : { $lte : ?1 } }, {'name' : { $eq: ?2 } }, {'description' : { $eq: ?3 } } ] }")
 	List<Product> findBySearch(@Param("min_price") Double min_price, @Param("max_price") Double max_price, String paramName, String paramDescription);
 	
 }
